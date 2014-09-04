9c330af717686f03ff4ac853bd1e3ce88cdd6e8f
Adam Legg <adam.legg@gmail.com>
2014-09-04 15:29:30 -0600

    controller and views vid complete

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140823215228) do
+ActiveRecord::Schema.define(version: 20140904210330) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cards", force: true do |t|
+    t.string   "front"
+    t.string   "back"
+    t.integer  "deck_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "cards", ["deck_id"], name: "index_cards_on_deck_id", using: :btree
 
   create_table "decks", force: true do |t|
     t.string   "name"
